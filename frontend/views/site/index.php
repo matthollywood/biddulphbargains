8922b031ea4a04a5200703ad44969954ab0e4dd1
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-01 22:13:28 +0100

    Front Page change

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -25,19 +25,15 @@ $this->title = 'Find local bargains at BiddulphBargains';
 				
 			</div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Who are we?</h2>
 
-                <p>Here I would like a picture of me or something tech related</p>
+                <p>BiddulphBargains believe that getting a great deal is never a bad thing. However in small towns such as Biddulph,
+				bargains and offers that are available are only spread through word of mouth and the occasional newsletter. Having all those
+				offers available on one website will not only help you find the best deals, but also help strengthen the towns economy.</p>
 
               
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p></p>
-
-                
-            </div>
+           
         </div>
 
     </div>
