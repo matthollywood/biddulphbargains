732ba08cceda1f06333146f97e01fcc51c36f72b
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-02 22:52:21 +0100

    change in title

--- frontend/views/site/addoffer.php
+++ frontend/views/site/addoffer.php
@@ -2,7 +2,7 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'Add your offer';
+$this->title = 'addOffer';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-addOffer">
