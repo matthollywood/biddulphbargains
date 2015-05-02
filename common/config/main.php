7d570d31247e4dea7ca73ce63c0d2cd1aa0b4890
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-02 11:25:05 +0100

    Database connection in main.php

--- common/config/main.php
+++ common/config/main.php
@@ -6,4 +6,21 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+	    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=db574676647.db.1and1.com;dbname=db574676647',
+            'username' => 'dbo574676647',
+            'password' => 'royksopp1',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
 ];
