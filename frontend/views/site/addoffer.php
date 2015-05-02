8ac241375eaa5b62fcbd4ad2709f9e36b854b9a8
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-02 15:35:59 +0100

    Added Offer Page to logged in users

---
+++ frontend/views/site/addoffer.php
@@ -0,0 +1,15 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+$this->title = 'Add your offer';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-Add offer">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Welcome to BiddulphBargains</p>
+	<div>
+		<p>To add your offer please fill in the form below</p>
+	</div>
+</div>
