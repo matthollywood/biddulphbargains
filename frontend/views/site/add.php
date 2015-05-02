68d573ab5c4bef7d1d2d43f561005127b19ce149
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-02 22:59:37 +0100

    change in breadcrumb

--- frontend/views/site/add.php
+++ frontend/views/site/add.php
@@ -2,7 +2,7 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'addOffer';
+$this->title = 'Add your Offer';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-addOffer">
