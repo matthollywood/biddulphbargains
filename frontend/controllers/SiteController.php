1940c6077f53bed0685079ee456328c537fb421d
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-06 21:36:39 +0100

    added terms and conditions

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -70,6 +70,11 @@ class SiteController extends Controller
     {
         return $this->render('index');
     }
+	
+	public function actionTermsAndConditions()
+	{
+		return $this->render('termsAndConditions');
+	}
 
     public function actionLogin()
     {
