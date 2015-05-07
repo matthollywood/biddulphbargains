cfdda32c9e07866b07739efdaaf2a043bbf9aa18
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-07 20:01:42 +0100

    changes to ts and cs

--- frontend/views/site/termsAndConditions.php
+++ frontend/views/site/termsAndConditions.php
@@ -2,14 +2,12 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-
-
 $this->title = 'Terms and Conditions';
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-<div class="TsandCs">
+<div class="termsAndConditions">
  <h1><?= Html::encode($this->title) ?></h1>
  
  <p>Welcome to BiddulphBargains</p>
