723736d33f93e03433352b4e70257c1184e316e7
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-09 16:42:46 +0100

    changes to index

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -16,29 +16,22 @@ $this->title = 'Find local bargains at BiddulphBargains';
     <div class="body-content">
 	
 	<div>
-	<h1> Here will be the search functionality</h1>
+	<h1> </h1>
 
         <div class="row">
             <div class="col-lg-4">
-                <h2> A little bit about me</h2>
+                <h2></h2>
 
-                <p>Welcome to BiddulphBargains, your one stop shop for all the best deals and offers in Biddulph Town Centre.</p>
+                <p>Welcome to BiddulphBargains, your one stop shop for all the best deals and offers in Biddulph Town Centre. BiddulphBargains believe that getting a great deal 
+				is never a bad thing. However in small towns such as Biddulph, bargains and offers that are available are only spread through word of mouth and the occasional newsletter. Having all those
+				offers available on one website will not only help you find the best deals, but also help strengthen the towns economy.</p>
 				
 			</div>
-            <div class="col-lg-4">
-                <h2>Who are we?</h2>
-
-                <p>BiddulphBargains believe that getting a great deal is never a bad thing. However in small towns such as Biddulph,
-				bargains and offers that are available are only spread through word of mouth and the occasional newsletter. Having all those
-				offers available on one website will not only help you find the best deals, but also help strengthen the towns economy.</p>
-
-              
-            </div>
-			
+           			
 			<div class="col-lg-4">
 			
 			
-				<p> Will have an image of biddulph high street here </p>
+				<p></p>
 			
 			</div>
 			
