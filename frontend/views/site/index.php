e8b9969f7a5a3548f8151ac3343a69f7e2bc1a0a
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-09 16:51:27 +0100

    more changes to index

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -27,7 +27,9 @@ $this->title = 'Find local bargains at BiddulphBargains';
                 <p>Welcome to BiddulphBargains, your one stop shop for all the best deals and offers in Biddulph Town Centre. BiddulphBargains believe that getting a great deal 
 				is never a bad thing. However in small towns such as Biddulph, bargains and offers that are available are only spread through word of mouth and the occasional newsletter. Having all those
 				offers available on one website will not only help you find the best deals, but also help strengthen the towns economy.</p>
-				
+				<p>There is no need to sign up to view these offers, simply use the search box above to find the type of deal or store you're looking for. Alternatively you can also use the category page
+				to find the different deals across each of the offer types. <br>
+				Don't forget to check out the events page for the latest news on what's going on in Biddulph</p>
 			</div>
            			
 
