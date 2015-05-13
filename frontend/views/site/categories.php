43afcfbf41b55b01a7e4bd4a8a39ed657284bf8f
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-13 20:57:51 +0100

    hail mary

--- frontend/views/site/categories.php
+++ frontend/views/site/categories.php
@@ -12,12 +12,9 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>Select the type of offer you're looking for to display all offers of that type in the area.</p>
 
     <?= ListView::widget([
-		'summary' => null,
-        'dataProvider' => $provider,
-		'itemView' => function($model)
-		
-		
-		{return'<div class="row">
+		'dataProvider' => $provider,
+		'itemView' => function($model),
+	{return'<div class="row">
 	<div class="col-md-4">
 		<div class="list-group">
 			<a href="#" class="list-group-item active">
@@ -27,6 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
 		</div>
 	</div>
 </div>';
-		}
+		},
+		'summary' => null
     ]); ?>
 </div>
