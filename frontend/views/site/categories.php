a45ae7ea153596cc0e7d4ec010c2a968d2135743
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-13 20:42:50 +0100

    give up :(

--- frontend/views/site/categories.php
+++ frontend/views/site/categories.php
@@ -15,9 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $provider,
 		'itemView' => function($model)
 		
-		{
-			$summary = null
-			return'<div class="row">
+		{return'<div class="row">
 	<div class="col-md-4">
 		<div class="list-group">
 			<a href="#" class="list-group-item active">
