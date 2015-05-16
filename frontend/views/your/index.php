c15e2c5f820c2f5634809f5f8216f8b0f0690474
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-16 20:48:49 +0100

    display search options

--- frontend/views/your/index.php
+++ frontend/views/your/index.php
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="your-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
         <?= Html::a('Add Offer', ['create'], ['class' => 'btn btn-success']) ?>
