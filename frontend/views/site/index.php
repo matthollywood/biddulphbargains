8ff896a84679bc70001cc627369d71175cf3c7b8
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-24 18:09:26 +0100

    didnt work

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -22,9 +22,11 @@ $this->title = 'Find local bargains at BiddulphBargains';
 
             <div class="span12">
                 <form action="categorieslanding" method="post">
-					<div class="span6">
+					<div class="row">
+					<div class="col-md-6">
 					<div class="form-group">
 			        <input type="text" name="keyword" placeholder="Search" class="form-control">
+					</div>
 					</div>
 					</div>
 					<div class="form-group">
