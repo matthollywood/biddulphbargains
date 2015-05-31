467a806e5827249146341f3a395e3086ead21c83
matthollywood <matthew_jones81@yahoo.co.uk>
2015-05-31 20:45:24 +0100

    wrong directory

--- backend/controllers/OffersController.php
+++ backend/controllers/OffersController.php
@@ -8,7 +8,7 @@ use backend\models\TblOffersSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
-use backend\controllers\User;
+use backend\models\User;
 /**
  * OffersController implements the CRUD actions for TblOffers model.
  */
