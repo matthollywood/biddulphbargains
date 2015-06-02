923e7a7129d8563b648685b6eef4f392f48840a9
matthollywood <matthew_jones81@yahoo.co.uk>
2015-06-02 20:34:17 +0100

    testing the search

--- backend/models/TblOffersSearch.php
+++ backend/models/TblOffersSearch.php
@@ -59,7 +59,7 @@ class TblOffersSearch extends TblOffers
 
         $query->andFilterWhere([
             'offer_id' => $this->offer_id,
-            'id' => $this->id,
+            'id' => $this->Yii::app->user->id;,
             'offer_start_date' => $this->offer_start_date,
             'offer_end_date' => $this->offer_end_date,
         ]);
