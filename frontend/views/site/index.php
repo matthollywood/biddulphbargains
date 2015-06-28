b4e516408405ddccdc83e2136ea049d66083e788
matthollywood <matthew_jones81@yahoo.co.uk>
2015-06-28 19:28:21 +0100

    removed test text and added divs

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -29,18 +29,19 @@ $this->title = 'Find local bargains at BiddulphBargains';
 					</div>
 					<div class="form-group">
 			            <?= Html::submitButton('Search for offers', ['class' => 'btn btn-primary', 'name' => 'submit']) ?>
-                  this is a test
+
 			        </div>
 		        </form>
           </div>
-
+          <br>
+        <div>
                 <p>Welcome to BiddulphBargains, your one stop shop for all the best deals and offers in Biddulph Town Centre. BiddulphBargains believe that getting a great deal
 				is never a bad thing. However in small towns such as Biddulph, bargains and offers that are available are only spread through word of mouth and the occasional newsletter. Having all those
 				offers available on one website will not only help you find the best deals, but also help strengthen the towns economy.</p>
 				<p>There is no need to sign up to view these offers, simply use the search box above to find the type of deal or store you're looking for. Alternatively you can also use the category page
 				to find the different deals across each of the offer types. <br>
 				Don't forget to check out the events page for the latest news on what's going on in Biddulph</p>
-
+      </div>
 
 
 
