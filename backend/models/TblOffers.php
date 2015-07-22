c6d7e222defaa24dced3ff87109d919cdbdd30c3
matthollywood <matthew_jones81@yahoo.co.uk>
2015-07-22 21:12:11 +0100

    missed active status from return from database

--- backend/models/TblOffers.php
+++ backend/models/TblOffers.php
@@ -33,8 +33,8 @@ class TblOffers extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['id', 'offer_type_id', 'offer_description', 'offer_start_date', 'offer_end_date','active_status'], 'required'],
-            [['store_user_id','id','active_status'], 'integer'],
+            [['id', 'offer_type_id', 'offer_description', 'offer_start_date', 'offer_end_date','active_status','active_status'], 'required'],
+            [['store_user_id','id','active_status','active_status'], 'integer'],
             [['offer_start_date', 'offer_end_date','offer_type_id'], 'safe'],
             [['offer_description'], 'string', 'max' => 8000]
         ];
