1c6d20ac2dcc40f1a15f08bd626a6bb85b0c8290
matthollywood <matthew_jones81@yahoo.co.uk>
2015-09-27 21:14:50 +0100

    changed label name but still not working

--- backend/views/offers/view.php
+++ backend/views/offers/view.php
@@ -33,8 +33,8 @@ $this->params['breadcrumbs'][] = $this->title;
             [                      // the offer name of the model
                 'label' => 'Offer Type',
                 'value' => $model->offerType->offer_type,
-                'label' =>   'active_status',
-                'value' => $model->offerStatus->active_status
+                'label' =>   'Active Status',
+                'value' => $model->offerStatus->active_status,
                        ],
 
             'offer_description',
