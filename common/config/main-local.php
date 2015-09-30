8837600cfb054d45d9a5b69206a7cc3020fe31b1
matthollywood <matthew_jones81@yahoo.co.uk>
2015-09-30 21:28:58 +0100

    changed SMTP settings

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -17,10 +17,10 @@ return [
             'useFileTransport' => false,
             'transport' => [
             'class' => 'Swift_SmtpTransport',
-            'host' => 'smtp.1and1.com',
+            'host' => 'auth.smtp.1and1.com',
             'username' => 'matthew.jones@biddulphbargains.co.uk',
             'password' => 'royksopp1',
-            'port' => '25',
+            'port' => '587025',
                         ],
         ],
 		'session' => [
