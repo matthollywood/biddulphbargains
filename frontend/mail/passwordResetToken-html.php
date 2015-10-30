ee15d0fc80079cb6bce579222f4346c0860b47d3
matthollywood <matthew_jones81@yahoo.co.uk>
2015-10-30 19:20:58 +0000

    wrong email

--- frontend/mail/passwordResetToken-html.php
+++ frontend/mail/passwordResetToken-html.php
@@ -7,9 +7,17 @@ use yii\helpers\Html;
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
 ?>
 <div class="password-reset">
+
+  <html>
+<body>
+  <img src="http://www.biddulphbargains.co.uk/css/images/original-logos-2015-Jun-8625-5800746">
+
     <p>Hello <?= Html::encode($user->username) ?>,</p>
 
     <p>Follow the link below to reset your password:</p>
 
     <p><?= Html::a(Html::encode($resetLink), $resetLink) ?></p>
+
+  </body>
+  </html>
 </div>
