33be55511822340b733aac4671a7c46d1d8c865f
matthollywood <matthew_jones81@yahoo.co.uk>
2015-11-01 20:09:00 +0000

    missed params

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -252,13 +252,13 @@ class SiteController extends Controller
         			'tbl_stores.store_name'])
                     ->from('tbl_offers')
                     ->join('LEFT OUTER JOIN', 'tbl_offer_types',
+                    'tbl_offers.offer_type_id = tbl_offer_types.offer_id')
                     ->join('LEFT OUTER JOIN','tbl_stores',
         				'tbl_offers.id =tbl_stores.store_id')
                     ->where('active_status =1 AND (offer_description LIKE :likeid OR offer_start_date = :id OR
                     offer_end_date = :id OR offer_type_id = :tid OR offer_type_id = :tid OR store_name LIKE :likeid)
                     AND offer_start_date < CURDATE()
                     AND offer_end_date >= CURDATE()',
-                    'tbl_offers.offer_type_id = tbl_offer_types.offer_id')
                     [':id' =>$id, ':likeid' => '%' . $id .'%',':tid'=>$tid])
                     ->all();
 
