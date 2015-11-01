6e5d5889a2ac626e9fe5a3a651739daf81f59466
matthollywood <matthew_jones81@yahoo.co.uk>
2015-11-01 20:05:04 +0000

    attempt at adding cat typ

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -248,6 +248,7 @@ class SiteController extends Controller
         			'tbl_offers.offer_start_date',
         			'tbl_offers.offer_end_date',
               'tbl_offers.offer_type_id',
+              'tbl_offer_types.offer_type'
         			'tbl_stores.store_name'])
                     ->from('tbl_offers')
                     ->join('LEFT OUTER JOIN','tbl_stores',
@@ -256,6 +257,8 @@ class SiteController extends Controller
                     offer_end_date = :id OR offer_type_id = :tid OR offer_type_id = :tid OR store_name LIKE :likeid)
                     AND offer_start_date < CURDATE()
                     AND offer_end_date >= CURDATE()',
+                    ->join('LEFT OUTER JOIN', 'tbl_offer_types',
+                    'tbl_offers.offer_type_id = tbl_offer_types.offer_id')
                     [':id' =>$id, ':likeid' => '%' . $id .'%',':tid'=>$tid])
                     ->all();
 
