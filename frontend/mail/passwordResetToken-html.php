777162a55eb2c61be7c8b4311736ba4d6eecf090
matthollywood <matthew_jones81@yahoo.co.uk>
2015-11-06 21:57:11 +0000

    added attributes to password reset email

--- frontend/mail/passwordResetToken-html.php
+++ frontend/mail/passwordResetToken-html.php
@@ -10,10 +10,12 @@ $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 't
 
   <html>
 <body>
-  <img src="http://www.biddulphbargains.co.uk/css/images/original-logos-2015-Jun-8625-5800746">
-
+  <a href="http://www.BiddulphBargains.co.uk">
+  <img src="http://www.biddulphbargains.co.uk/css/images/original-logos-2015-Jun-8625-5800746" alt="BiddulphBargains" width = "700" height "231" align="middle">
+</a>
     <p>Hello <?= Html::encode($user->username) ?>,</p>
-
+    <p>It loosk like you've missed placed your password. Not to worry!</P>
+    <p>It's really easy to reset your password,<p>
     <p>Follow the link below to reset your password:</p>
 
     <p><?= Html::a(Html::encode($resetLink), $resetLink) ?></p>
